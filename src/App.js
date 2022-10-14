938537391aa6db269592a11803c7dfe348bbc190
imanuelyosua77 <113983223+imanuelyosua77@users.noreply.github.com>
2022-10-14 16:39:38 +0800

    App.js add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import Header from './components/Header';
+import Contact from './components/Contact';
+import ContactForm from './components/ContactForm';
+// import TestingData from './components/TestingData';
 
 // Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from './data/contacts.json';
+import { useState } from 'react';
+
 const App = () => {
+
+  const data = contactsJSON.map((resource) => {
+    return resource
+  })
+ 
+  const [kontak, setKontak] = useState(data)
+
+  function spreadData(newContact) {
+    setKontak([...kontak, newContact])
+  }
+
+
+  // spreadData();
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
   // https://mui.com/material-ui/react-list/#folder-list
@@ -10,8 +29,12 @@ const App = () => {
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
 
+
   return (
     <div className="App">
+      <Header />
+      <ContactForm spreadData={spreadData} />
+      <Contact data={kontak} />
     </div>
   );
 };
