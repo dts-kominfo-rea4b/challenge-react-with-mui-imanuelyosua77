8d0d24584121a0508155c937f8316826f11cba93
imanuelyosua77 <113983223+imanuelyosua77@users.noreply.github.com>
2022-10-14 16:44:03 +0800

    simple List

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,40 +2,33 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import Divider from '@mui/material/Divider';
+// import ListItemText from '@mui/material/ListItemText';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+// import Avatar from '@mui/material/Avatar';
+// import { useState } from 'react';
+// import Typography from '@mui/material/Typography';
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     return (
-        <>
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                {data.map((kontak, index) => {
-                    return (
-                        <>
-                            <ListItem key={index} alignItems="flex-start">
-                                <ListItemAvatar><Avatar alt="Avatar" src={kontak.photo} /></ListItemAvatar>
-                                <ListItemText primary={kontak.name}
-                                    secondary={
-                                        <>
-                                            <Typography>{kontak.phone}</Typography>
-                                            <Typography>{kontak.email}</Typography>                                 
-                                         </>
-                                    }
-                                />
-                            </ListItem>
-                            <Divider variant="inset" component="li" />
-                        </>
+        <div>
+            <ul sx={{display:'block'}}>
+                {data.map((kontak, index) => { 
+                return(
+                    <li key={index} sx={{display:'block'}}>
+                        <img src={kontak.photo}></img>
+                        <p>{kontak.name}</p>
+                        <p>{kontak.phone}</p>
+                        <p>{kontak.email}</p>
+                    </li>
                     )
                 })}
-            </List>
-        </>
+            </ul>
+        </div>
     );
 };
 
